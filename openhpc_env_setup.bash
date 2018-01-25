ab08fc217898d2eab4302a598c3545addc8de07c
Spencer Myers <spencer.myers@inl.gov>
2018-01-25 15:43:14 -0700

    sshd file got reverted back to system file

--- openhpc_env_setup.bash
+++ openhpc_env_setup.bash
@@ -10,7 +10,7 @@ ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ""
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 
 hostname=$(hostname -s)
-perl -i -pe 's/\s*\#?\s*PasswordAuthentication\s+\S+\s*$/PasswordAuthentication yes\n/g' /etc/ssh/sshd_config
+perl -i -pe 's/\s*\#?\s*PasswordAuthentication\s+\S+\s*$/PasswordAuthentication yes\n/g' /root/CRI_XCBC/roles/pre_ohpc/templates/sshd_config.j2
 perl -i -pe "s/tr\s+.*?\)/perl -pe 's\/\\\n\/,\/g' | perl -pe 's\/,\\s*\\$\/\/')/" /root/CRI_XCBC/roles/nodes_vivify/tasks/main.yml
 perl -i -pe 's/(^\s+-\s*hosts:.*)/$1\n   ignore_errors: yes\n/' /root/CRI_XCBC/headnode.yml
 perl -i -pe "s/^headnode(.*)/${hostname}\$1/" /root/CRI_XCBC/inventory/headnode
