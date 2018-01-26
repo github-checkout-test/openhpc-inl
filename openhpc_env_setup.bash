422fc66eddeb0d4ccf089790b2f61c70a0efb4e2
Spencer Myers <spencer.myers@inl.gov>
2018-01-26 13:08:29 -0700

    Reverted the tr command for newlines but added a pause for slurmd to come up

--- openhpc_env_setup.bash
+++ openhpc_env_setup.bash
@@ -11,7 +11,8 @@ cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 
 hostname=$(hostname -s)
 perl -i -pe 's/\s*\#?\s*PasswordAuthentication\s+\S+\s*$/PasswordAuthentication yes\n/g' /root/CRI_XCBC/roles/pre_ohpc/templates/sshd_config.j2
-perl -i -pe "s/tr\s+.*?\)/perl -pe 's\/\\\n\/,\/g' | perl -pe 's\/,\\s*\\$\/\/')/" /root/CRI_XCBC/roles/nodes_vivify/tasks/main.yml
+#perl -i -pe "s/tr\s+.*?\)/perl -pe 's\/\\\n\/,\/g' | perl -pe 's\/,\\s*\\$\/\/')/" /root/CRI_XCBC/roles/nodes_vivify/tasks/main.yml
+perl -0777 -i -pe 's/(enable slurmd.*?)name/$1name: Wait\n     wait_for: timeout=10\n\n   - name/sg' /root/CRI_XCBC/roles/nodes_vivify/tasks/main.yml
 perl -i -pe 's/(^\s+-\s*hosts:.*)/$1\n   ignore_errors: yes\n/' /root/CRI_XCBC/headnode.yml
 perl -i -pe "s/^headnode(.*)/${hostname}\$1/" /root/CRI_XCBC/inventory/headnode
 
